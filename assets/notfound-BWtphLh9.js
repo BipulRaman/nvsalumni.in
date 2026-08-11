4d9fd1f2de05d77236c0c991e769e990eff17866
BipulRaman <bipulraman@gmail.com>
2026-08-11 11:56:56 +0000

    Update from https://github.com/BipulRaman/AlumniApp/commit/0a7275e42770b144011d26d668eaf7f949ac80ae

--- assets/notfound-BWtphLh9.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./NotFound-C74UVUJ4.js";var t=e;export{t as component};
--- assets/profile-CFRcsHtx.js
+++
@@ -1,1 +0,0 @@
-import{At as e}from"./mui-core-Bi-YaxUU.js";import{s as t}from"./Routes-D-2OQcZp.js";import{t as n}from"./PageTabs-Czu5_GcE.js";import{t as r}from"./ProfileView-BgcTLeFW.js";var i=e();function a(){return(0,i.jsxs)(i.Fragment,{children:[(0,i.jsx)(n,{tabs:t}),(0,i.jsx)(r,{})]})}export{a as component};
