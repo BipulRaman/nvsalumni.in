9e747ed93b7cd558de8eb05b2e38d274fcb3a100
BipulRaman <bipulraman@gmail.com>
2026-08-11 06:22:02 +0000

    Update from https://github.com/BipulRaman/AlumniApp/commit/7b11b5666eb8bd253ec04d7fbec49e63c8ff6427

---
+++ assets/register-D_pBHXrl.js
@@ -0,0 +1,1 @@
+import{At as e}from"./mui-core-Bi-YaxUU.js";import{n as t}from"./Routes-Du-N9HeF.js";import{AttendeeSearch as n}from"./AttendeeSearch-DUBFOuSX.js";import{t as r}from"./PageTabs-DazqowH2.js";var i=e();function a(){return(0,i.jsx)(r,{tabs:t,children:(0,i.jsx)(n,{initialMode:`register`})})}export{a as component};
